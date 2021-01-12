938a5e2a50998f8958039c5c9a1e2c53912d2830
gimbolam <3017129@naver.com>
2021-01-12 20:01:05 +0900

    보람 - 리뷰, q&a 프레그먼트 까지 리뷰는 기능 아직 끝 아님

---
+++ app/src/main/java/com/example/supia/Activities/Product/ReviewFragment.java
@@ -0,0 +1,30 @@
+package com.example.supia.Activities.Product;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import com.example.supia.R;
+
+class ReviewFragment extends Fragment {
+
+    String urlIp;
+    int productNo;
+
+    public ReviewFragment(String urlIp, int productNo) {
+        this.urlIp = urlIp;
+        this.productNo = productNo;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_review, container, false);
+    }
+}//끄읕
---
+++ app/src/main/java/com/example/supia/Activities/Product/WriteQnAActivity.java
@@ -0,0 +1,15 @@
+package com.example.supia.Activities.Product;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.supia.R;
+
+public class WriteQnAActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_qna);
+    }
+}
