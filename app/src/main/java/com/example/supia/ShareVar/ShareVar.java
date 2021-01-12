c92d290b1d2188deaaa9705093ad7aea1e5a2b6e
Tunapaw <peapea1@naver.com>
2021-01-13 07:35:01 +0900

    tuna_0113_MySQL 날짜 표시, drawable 파일 추가(아직 적용x)/EventDecorator page에 주석 부분 지우지 말아주세요

--- app/src/main/java/com/example/supia/ShareVar/ShareVar.java
+++ app/src/main/java/com/example/supia/ShareVar/ShareVar.java
@@ -2,15 +2,21 @@ package com.example.supia.ShareVar;
 
 public class ShareVar {
 
-    public static String urlIp = "192.168.35.147";//핫팬츠
+//    public static String urlIp = "192.168.35.147";//핫팬츠
 //    public static String urlIp = "222.106.89.206";//불량감자
 //    public static String urlIp = "172.30.1.19";//고집불통
 //    public static String urlIp = "192.168.0.100";//김보츄
-//    public static String urlIp = "192.168.2.2";//튜나(갈라파고스 코더)
+    public static String urlIp = "192.168.1.5";//튜나(갈라파고스 코더)
 
 
 
     public static String sharvarUserId;
+
+    //tuna 추가
+    public static String calendarsharvarStartdate;
+    public static String calendarsharvarFinishdate;
+    public static String calendarsharvarDeliverydate;
+    public static String calendarsharvarBirthdate;
 
 
     //inwoo 추가
