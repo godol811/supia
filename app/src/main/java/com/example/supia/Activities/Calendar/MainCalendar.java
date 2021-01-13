1dd51cb6a6d49f918e8da893981ac42e5fdf86df
Tunapaw <peapea1@naver.com>
2021-01-13 20:53:17 +0900

    하진-01.12-DTO추가

--- app/src/main/java/com/example/supia/Activities/Calendar/MainCalendar.java
+++ app/src/main/java/com/example/supia/Activities/Calendar/MainCalendar.java
@@ -111,7 +111,7 @@ public class MainCalendar extends FragmentActivity {
         //materialCalendarView_main.setBackgroundResource(R.drawable.calendar_background_total);
 
         materialCalendarView_main.setSelectedDate(CalendarDay.from(intdelyear, intdelmonth, intdelday));
-        materialCalendarView_main.addDecorator(new EventDecoratorDraw(Collections.singleton(CalendarDay.from(intdelyear, intdelmonth, intdelday))));
+        materialCalendarView_main.addDecorator(new EventDecorator(Color.YELLOW, Collections.singleton(CalendarDay.from(intdelyear, intdelmonth, intdelday))));
 
         materialCalendarView_main.setSelectedDate(CalendarDay.from(intbiryear, intbirmonth, intbirday));
         materialCalendarView_main.addDecorator(new EventDecorator(Color.BLUE, Collections.singleton(CalendarDay.from(intbiryear, intbirmonth, intbirday))));
