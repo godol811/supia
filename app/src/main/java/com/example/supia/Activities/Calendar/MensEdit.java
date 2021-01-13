90b253f2c32c16438ad67b50f4af8af89ba66401
Tunapaw <peapea1@naver.com>
2021-01-13 23:48:28 +0900

    하진-01.13-null

--- app/src/main/java/com/example/supia/Activities/Calendar/MensEdit.java
+++ app/src/main/java/com/example/supia/Activities/Calendar/MensEdit.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -22,13 +23,13 @@ public class MensEdit extends Dialog {
     final static String TAG = "캘린더_월경일선택_material";
     private final Context context;
     MaterialCalendarView materialCalendarView;
-    TextView textView;
-    ImageButton cancle;
+    TextView tvToday;
     Button btnInsert, btnUpdate, btnDelete;
     ImageButton btnCancle;
     String menstruationStart, menstruationEnd;
-
-    Date today,startdate,finishdate;
+    public String CurrentStartDay, LastFinishDay;
+    int year, month, dayOfMonth;
+    String pickDate;
 
     myDBHelper myDBHelper;
     SQLiteDatabase sqlDB;
@@ -47,39 +48,99 @@ public class MensEdit extends Dialog {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_mens_edit);
 
-        btnInsert = findViewById(R.id.btn_insert_datepicker_calendar);
-        btnUpdate = findViewById(R.id.btn_update_datepicker_calendar);
-        btnDelete = findViewById(R.id.btn_delete_datepicker_calendar);
-        btnCancle = findViewById(R.id.btn_cancle_datepicker_calendar);
+        btnInsert = findViewById(R.id.btn_insert_mensedit_calendar);
+        btnUpdate = findViewById(R.id.btn_update_mensedit_calendar);
+        btnDelete = findViewById(R.id.btn_delete_mensedit_calendar);
+        btnCancle = findViewById(R.id.btn_cancle_mensedit_calendar);
+        tvToday = findViewById(R.id.tv_today_mensedit_calendar);
 
-        cancle = findViewById(R.id.btn_cancle_mensedit_calendar);
-        cancle.setOnClickListener(new View.OnClickListener() {
+        tvToday.setText("");
+
+        //year = Integer.parseInt(String.valueOf(CalendarDay.today()));
+        year = 2021;
+        month = 01;
+        dayOfMonth = 15;//데이트피커에서 날짜를 가져옴
+        checkDay(year, month, dayOfMonth);//가져온 날짜에 하이픈을 첨가하여 "yyyy-mm-dd" 형태로 만듬
+
+//        materialCalendarView.setOnDateChangedListener((view, year1, monthOfYear, dayOfMonth1) -> {
+//            checkDay( year1, monthOfYear, dayOfMonth1);
+//        });
+
+        menstruationStart = checkDay(year, month, dayOfMonth);
+        Log.v(TAG, "오오오" + menstruationStart);
+        menstruationEnd = checkDay(year, month, dayOfMonth);
+        Log.v(TAG, "오오오" + menstruationEnd);
+
+        btnCancle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dismiss();
             }
         });
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDB = myDBHelper.getWritableDatabase();
+                sqlDB.execSQL("INSERT INTO supiamensterm (mStart, mEnd)VALUES ( '" + menstruationStart + "' , '" + menstruationEnd + "');");
+                sqlDB.close();
+            }
+        });
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDB = myDBHelper.getWritableDatabase();
+                sqlDB.execSQL("UPDATE INTO supiamensterm (mStart, mEnd)VALUES ( '" + menstruationStart + "' , '" + menstruationEnd + "');");
+                sqlDB.close();
+            }
+        });
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDB = myDBHelper.getWritableDatabase();
+                sqlDB.execSQL("DELETE INTO supiamensterm (mStart, mEnd)VALUES ( '" + menstruationStart + "' , '" + menstruationEnd + "');");
+                sqlDB.close();
+
+            }
+        });
 
 
-       // Date [] dates = materialCalendarView.getSelectedDates();
+
 
         materialCalendarView = findViewById(R.id.mensedit_calendar);
         materialCalendarView.setSelectionMode(MaterialCalendarView.SELECTION_MODE_RANGE);
 
         materialCalendarView.getCurrentDate();
-        materialCalendarView.getSelectedDates();
+
 
 
 
     }
 
-
+    public String checkDay(int cYear, int cMonth, int cDay) {
+        pickDate = cYear + "-" + (cMonth + 1) + "" + "-" + cDay;
+        Log.v("TAG", "오오오" + pickDate);
+        return pickDate;
+    }
 
     public class myDBHelper extends SQLiteOpenHelper {
+//        public static final String DATABASE_NAME = "reminders.db";
+//        public static final String T_1 = "tbl_users";
+//        public static final String T1_COL_1 = "ID";
+//        public static final String T1_COL_2 = "FIRST_NAME";
+//        public static final String T1_COL_3 = "MIDDLE_INITIAL";
+//        public static final String T1_COL_4 = "LAST_NAME";
+//        public static final String T1_COL_5 = "PHONE";
+//        public static final String T1_COL_6 = "EMAIL";
+//        public static final String T1_COL_7 = "USERNAME";
+//        public static final String T1_COL_8 = "PASSWORD";
         public myDBHelper(Context context) {
             super(context, "supia", null, 1);
         }
-
+//
+//
+//        Object obj = myDBHelper..get();
+//        dates = (HashSet<CalendarDay>) obj;
         @Override
         public void onCreate(SQLiteDatabase db) {
             db.execSQL("CREATE TABLE supiamensterm (mStart DATE(20), mEnd DATE(20));");
@@ -91,5 +152,6 @@ public class MensEdit extends Dialog {
             onCreate(db);
 
         }
+
     }
 }
