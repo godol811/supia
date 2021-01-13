628cb84321f6fd6e39c1a1c696357b7a4761b7f7
gimbolam <3017129@naver.com>
2021-01-14 01:55:52 +0900

    21.01.14 am 1:55 리뷰 에러 해결중

--- app/src/main/java/com/example/supia/Activities/Product/ReviewFragment.java
+++ app/src/main/java/com/example/supia/Activities/Product/ReviewFragment.java
@@ -1,6 +1,7 @@
 package com.example.supia.Activities.Product;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,23 +9,69 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.supia.Adapter.Product.ProductReviewListAdapter;
+import com.example.supia.Dto.Product.ProductReviewDto;
+import com.example.supia.NetworkTask.MyPage.MyReviewInsertNetworkTask;
 import com.example.supia.R;
+
+import java.util.ArrayList;
 
 public class ReviewFragment extends Fragment {
 
     String urlIp;
     int productNo;
 
+
+    private RecyclerView recyclerView;
+    private ProductReviewListAdapter adapter;
+    private ArrayList<ProductReviewDto> list;
+    RecyclerView.LayoutManager reviewLayoutManager = null;
+
+
+
     public ReviewFragment(String urlIp, int productNo) {
         this.urlIp = urlIp;
         this.productNo = productNo;
     }
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_review, container, false);
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_review, container, false);
+
+
+        list = new ArrayList<ProductReviewDto>();
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.rv_review_fragmentqna);
+        recyclerView.setHasFixedSize(true);
+        reviewLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(reviewLayoutManager);
+
+
+        connectGetData();
+        return rootView;
+
+    }
+
+    private void connectGetData() {
+        try {
+
+            String reviewUrl = "http://" + urlIp + ":8080/test/supiaProductReviewList.jsp?productNo="+productNo;
+            MyReviewInsertNetworkTask networkTask1 = new MyReviewInsertNetworkTask(getActivity(), reviewUrl, "select");
+            Object obj = networkTask1.execute().get();
+            list = (ArrayList<ProductReviewDto>) obj;
+            adapter = new ProductReviewListAdapter(getContext(), R.layout.activity_my_order, list);
+            recyclerView.setAdapter(adapter);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }//끄읕
