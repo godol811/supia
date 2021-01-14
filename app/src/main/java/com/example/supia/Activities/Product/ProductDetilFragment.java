9bbf6aa84edf165ca1824d6f73f2e290e1c30362
gimbolam <3017129@naver.com>
2021-01-14 23:39:47 +0900

    보람 - 21.01.14 pm 11:39 프로덕트 디테일 선호도 디버깅전 풀 받아야함

--- app/src/main/java/com/example/supia/Activities/Product/ProductDetilFragment.java
+++ app/src/main/java/com/example/supia/Activities/Product/ProductDetilFragment.java
@@ -15,6 +15,8 @@ import androidx.annotation.Nullable;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.supia.NetworkTask.Product.NetworkTaskBuyCount;
+import com.example.supia.NetworkTask.Product.NetworkTaskPreference;
 import com.example.supia.R;
 
 import java.text.DecimalFormat;
@@ -100,36 +102,124 @@ public class ProductDetilFragment extends Fragment {
         text50 = view.findViewById(R.id.text50);
         progressBar50 = view.findViewById(R.id.progress50);
 
-        setProgressBar();
+        connectGetDataTen();
+        connectGetDataTwenty();;
+        connectGetDataThirty();
+        connectGetDataForty();
+        connectGetDataFifty();
 
 
         return view;
     }
 
-    public void setProgressBar() {
-        String inputStr10 = text10.getText().toString().trim();
-        int input10 = Integer.parseInt(inputStr10);     // 문자 -> 숫자 변환
 
-        String inputStr20 = text20.getText().toString().trim();
-        int input20 = Integer.parseInt(inputStr20);     // 문자 -> 숫자 변환
+    //------ 10대 선호도
+    private void connectGetDataTen() {
+        try {
 
-        String inputStr30 = text30.getText().toString().trim();
-        int input30 = Integer.parseInt(inputStr30);     // 문자 -> 숫자 변환
+            String buyUrl = "http://" + urlIp + ":8080/test/supiaPreferenceTen.jsp?productNo="+productNo;
+            NetworkTaskPreference networkTask2 = new NetworkTaskPreference(getContext(), buyUrl, "select");
+            Object obj10 = networkTask2.execute().get();
+            String birth = (String) obj10;
+            int birthData = Integer.parseInt(birth)*10;
+            String Preference10 = Integer.toString(birthData);
+            text10.setText(Preference10); //json 파싱
+            String inputStr10 = text10.getText().toString().trim(); //프로그래스바
+            int input10 = Integer.parseInt(inputStr10);
+            progressBar10.setProgress(input10);
 
-        String inputStr40 = text40.getText().toString().trim();
-        int input40 = Integer.parseInt(inputStr40);     // 문자 -> 숫자 변환
-
-        String inputStr50 = text50.getText().toString().trim();
-        int input50 = Integer.parseInt(inputStr50);     // 문자 -> 숫자 변환
-
-        progressBar10.setProgress(input10);         // 0~ 100 까지 설정
-        progressBar20.setProgress(input20);         // 0~ 100 까지 설정
-        progressBar30.setProgress(input30);         // 0~ 100 까지 설정
-        progressBar40.setProgress(input40);         // 0~ 100 까지 설정
-        progressBar50.setProgress(input50);         // 0~ 100 까지 설정
-
-
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+//--------
+
+    //------ 20대 선호도
+    private void connectGetDataTwenty() {
+        try {
+
+            String buyUrl = "http://" + urlIp + ":8080/test/supiaPreferenceTwenty.jsp?productNo="+productNo;
+            NetworkTaskPreference networkTask2 = new NetworkTaskPreference(getContext(), buyUrl, "select");
+            Object obj20 = networkTask2.execute().get();
+            String birth = (String) obj20;
+            int birthData = Integer.parseInt(birth)*10;
+            String Preference20 = Integer.toString(birthData);
+            text20.setText(Preference20); //json 파싱
+            String inputStr20 = text20.getText().toString().trim(); //프로그래스바
+            int input20 = Integer.parseInt(inputStr20);
+            progressBar20.setProgress(input20);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+//--------
+
+    //------ 30대 선호도
+    private void connectGetDataThirty() {
+        try {
+
+            String buyUrl = "http://" + urlIp + ":8080/test/supiaPreferenceThirty.jsp?productNo="+productNo;
+            NetworkTaskPreference networkTask2 = new NetworkTaskPreference(getContext(), buyUrl, "select");
+            Object obj30 = networkTask2.execute().get();
+            String birth = (String) obj30;
+            int birthData = Integer.parseInt(birth)*10;
+            String Preference30 = Integer.toString(birthData);
+            text10.setText(Preference30); //json 파싱
+            String inputStr30 = text30.getText().toString().trim(); //프로그래스바
+            int input30 = Integer.parseInt(inputStr30);
+            progressBar30.setProgress(input30);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+//--------
+
+    //------ 40대 선호도
+    private void connectGetDataForty() {
+        try {
+
+            String buyUrl = "http://" + urlIp + ":8080/test/supiaPreferenceForty.jsp?productNo="+productNo;
+            NetworkTaskPreference networkTask2 = new NetworkTaskPreference(getContext(), buyUrl, "select");
+            Object obj40 = networkTask2.execute().get();
+            String birth = (String) obj40;
+            int birthData = Integer.parseInt(birth)*10;
+            String Preference40 = Integer.toString(birthData);
+            text40.setText(Preference40); //json 파싱
+            String inputStr40 = text40.getText().toString().trim(); //프로그래스바
+            int input40 = Integer.parseInt(inputStr40);
+            progressBar40.setProgress(input40);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+//--------
+
+    //------ 50대 선호도
+    private void connectGetDataFifty() {
+        try {
+
+            String buyUrl = "http://" + urlIp + ":8080/test/supiaPreferenceFifty.jsp?productNo="+productNo;
+            NetworkTaskPreference networkTask2 = new NetworkTaskPreference(getContext(), buyUrl, "select");
+            Object obj50 = networkTask2.execute().get();
+            String birth = (String) obj50;
+            int birthData = Integer.parseInt(birth)*10;
+            String Preference50 = Integer.toString(birthData);
+            text50.setText(Preference50); //json 파싱
+            String inputStr50 = text50.getText().toString().trim(); //프로그래스바
+            int input50 = Integer.parseInt(inputStr50);
+            progressBar50.setProgress(input50);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+//--------
+
+
+
 
 
 
