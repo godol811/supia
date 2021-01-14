518190ebdbb563bc80d3ffb16482a721c6f21b3c
Tunapaw <peapea1@naver.com>
2021-01-14 22:07:18 +0900

    하진_01.14_xml수정완, sqlite 보류, mysql 월경일업데이트 수정중(xml완)

--- app/src/main/java/com/example/supia/Activities/Calendar/mensUpdate.java
+++ app/src/main/java/com/example/supia/Activities/Calendar/mensUpdate.java
@@ -6,13 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 
+import com.example.supia.Activities.Login.UserDataQuestion2;
+import com.example.supia.Activities.Login.UserDataQuestion3;
 import com.example.supia.R;
+import com.example.supia.ShareVar.ShareVar;
 
 public class mensUpdate extends Dialog {
 
     private Context context;
     Button btnnext;
+    DatePicker datePicker;
 
     public mensUpdate(Context context){
         super(context);
@@ -25,10 +30,13 @@ public class mensUpdate extends Dialog {
         setContentView(R.layout.activity_calendar_updata_start);
 
         btnnext = findViewById(R.id.btn_next_mensupdate);
+        datePicker = findViewById(R.id.maincalendar_mens_start_update);
 
         btnnext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //ShareVar.updatemensstartdate = datePicker.get;
+
                 mensUpdate2 dialog = new mensUpdate2(getContext());
                 dialog.show();
                 dismiss();
